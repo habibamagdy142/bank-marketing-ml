10c2a5bcaddf461e6726a595f687f3978a9a4116
habibamagdy142 <habibamagdy82@gmail.com>
2026-08-30 00:55:33 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import joblib
 import pandas as pd
 
 # Load the trained model and preprocessor
-preprocessor, model = joblib.load("bank_marketing_model_compressed.pkl")
+preprocessor, model = joblib.load("bank_marketing_model_final.pkl")
 
 st.title("Bank Marketing Prediction")
 st.write("Predict whether a customer will subscribe to a term deposit.")
